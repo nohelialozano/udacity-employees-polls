8e44126913dfc414d5816142f1df141b8593a9ea
Nohelia Lozano <nohelia.lozano@corel.com>
2023-08-23 16:15:34 -0400

    Adding suggested solution Login

--- src/utils/Login.test.js
+++ src/utils/Login.test.js
@@ -4,15 +4,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { act } from 'react-dom/test-utils';
 import Login from "../components/Login";
 
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "../reducers"; // index.js where I combine the reducers
+import middleware from "../middleware"; // index.js inside the middleware folder
+
 describe('Login', () => {
     it('will match snapshot', async () => {
       let component;
+      const store = createStore(reducer, middleware);
   
       act(() => {
         component = render(
+          <Provider store={store}>
           <Router>
             <Login />
           </Router>
+          </Provider>
         );
       });
   
@@ -20,13 +28,4 @@ describe('Login', () => {
         expect(component).toMatchSnapshot();
       });
     });
-
-    /*it('will display an error if the user and password do not match the records', () => {
-        var component = render(<Login />);
-        fireEvent.change(component.getByTestId("user-input"), { target: { value: 'tylermcginnis' } });
-        fireEvent.change(component.getByTestId("password-input"), { target: { value: 'abc321' } });
-        var submitButton = component.getByTestId("submit-button");
-        fireEvent.click(submitButton);
-        expect(component.getByTestId('error-message')).toBeInTheDocument();
-    });*/
 });
